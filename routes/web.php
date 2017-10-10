50237a0772e04bb809b2834092aa55db2489eb53
Men phors <menphorsqty@gmail.com>
2017-10-10 23:42:09 +0700

    Merge branch 'soklim'
    # Conflicts:
    #	composer.lock
    #	public/css/bootstrap-theme.css
    #	public/css/bootstrap-theme.css.map
    #	public/css/bootstrap-theme.min.css
    #	public/css/bootstrap.css
    #	public/css/bootstrap.css.map
    #	public/css/bootstrap.min.css
    #	public/images/logo.png
    #	public/js/bootstrap.js
    #	public/js/bootstrap.min.js
    #	resources/assets/sass/app.scss
    #	routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,29 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 Route::get('/', function () {
     return view('index');
 });
+Route::group(['prefix' => 'language'], function () {
+    Route::post('/', [
+        'Middleware' => 'LanguageSwitcher',
+        'uses' => 'LanguageController@index'
+    ]);
+});
+
 Route::group(['prefix' => 'view'], function () {
     Route::get('/order-detail', ['as' => 'product', 'uses' => 'ProductController@index']);
 	Route::get('/proceed', ['as' => 'product', 'uses' => 'ProductController@proceed']);
 
 });
 
-
-
-Auth::routes();
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', ['as' => 'admin', 'uses' => 'AdminsController@index']);
+    Route::get('/edit/{id?}', ['as' => 'view_products', 'uses' => 'AdminsController@view_product']);
+    Route::get('/delete/{id}', ['as' => 'view_products', 'uses' => 'AdminsController@delete_product']);
+    Route::get('/add_product', ['as' => 'add_product', 'uses' => 'AdminsController@add_product']);
+});
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'categories'], function () {
